349c5d48e754f4b5c86c2eec9ab56ca9dff052dd
junpei5060 <junpei.nakazawa.5060@gmail.com>
2020-10-27 07:38:22 +0900

    “写真投稿”

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,5 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
 Route::post('/login', 'Auth\LoginController@login')->name('login');
 Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('/user', fn() => Auth::user())->name('user');
+Route::post('/photos', 'PhotoController@create')->name('photo.create');
